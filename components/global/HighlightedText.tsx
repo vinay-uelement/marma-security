5d5a526248e53fcbda3856e3c4b72302ad0bb742
Vinay Khairnar <vinay@uelement.in>
2026-03-20 15:35:03 +0530

    added card styling

--- components/global/HighlightedText.tsx
+++ components/global/HighlightedText.tsx
@@ -10,7 +10,7 @@ interface HighlightedTextProps {
 export default function HighlightedText({
     text,
     className = "text-white font-bold",
-    imageClassName = "bottom-[-22px] md:bottom-[-15px] left-3/4 -translate-x-1/2 w-[70%]"
+    imageClassName = " left-3/4 -translate-x-1/2 -translate-y-1/2 w-[70%]"
 }: HighlightedTextProps) {
     return (
         <span className={`relative inline-block z-10 ${className}`}>
