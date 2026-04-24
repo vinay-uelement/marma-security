56069c40a4190b56ef6b70a7433b8ae44d257cb3
Vinay Khairnar <vinay@uelement.in>
2026-04-24 19:20:26 +0530

    feat: add marmasec.com to allowed image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'marmasec.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'marma-security.vercel.app',
       },
     ],
