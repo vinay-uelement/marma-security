0a4abb35de901cd4b257e69c1ef67793584d9296
Vinay Khairnar <vinay@uelement.in>
2026-06-18 16:19:06 +0530

    refactor: move account deletion link and update footer address grid layout

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -196,6 +196,12 @@ export default function Footer() {
                   >
                     Partners
                   </Link>
+                  <Link
+                    href="/account-deletion"
+                    className="hover:text-white/70 transition-colors"
+                  >
+                    Account Deletion
+                  </Link>
                 </div>
 
                 {/* Column 2 */}
@@ -224,19 +230,14 @@ export default function Footer() {
                   >
                     Privacy
                   </Link>
-                  <Link
-                    href="/account-deletion"
-                    className="hover:text-white/70 transition-colors"
-                  >
-                    Account Deletion
-                  </Link>
+
                 </div>
 
                 {/* Column 3 - Contact Links */}
                 <div className="flex flex-col gap-8 footer-contact-text text-[9px] pl-1 break-all col-span-2">
-                  <div className="flex flex-wrap gap-8 w-full justify-between pr-4">
+                  <div className="grid grid-cols-2 gap-4 w-full pr-4">
                     {/* USA Column */}
-                    <div className="flex flex-col gap-3 w-[45%]">
+                    <div className="flex flex-col gap-3 min-w-0">
                       <div className="flex items-start gap-1 break-all sm:break-normal">
                         <MapPin size={16} className="shrink-0 mt-[2px]" />
                         <span>
@@ -244,8 +245,8 @@ export default function Footer() {
                           <br />
                           Marma Security Inc.,
                           <br />
-                          180 Promenade <br /> Ste. 300,
-                          Sacramento, <br /> CA - 95834
+                          180 Promenade Ste. 300,
+                          Sacramento, CA - 95834
                         </span>
                       </div>
                       <a href="tel:+14085828962" className="flex items-center gap-1.5 hover:opacity-80 transition-opacity">
@@ -257,7 +258,7 @@ export default function Footer() {
                     </div>
 
                     {/* India Column */}
-                    <div className="flex flex-col gap-3 w-[45%]">
+                    <div className="flex flex-col gap-3 min-w-0">
                       <div className="flex items-start gap-1 break-all sm:break-normal">
                         <MapPin size={16} className="shrink-0 mt-[2px]" />
                         <span>
